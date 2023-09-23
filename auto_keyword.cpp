80a9f91f216b1bf90d0eac982291911e1845726e
Zayn <mzainabbas77@gmail.com>
2023-09-23 22:52:34 +0500

    auto_keyword

---
+++ auto_keyword.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    auto x = 10;
+    cout << x << endl;
+    // x = "hello"; // Error......but I still don't get it...
+    cout << x << endl;
+}
